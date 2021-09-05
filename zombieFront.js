9789be833dd7c30101c51cf684715f4004cc53d7
Pablo Chanivet García <38567853+PabloChanivet@users.noreply.github.com>
2021-09-05 11:05:03 +0200

    Nueva implementación
    Nueva implementación del front

--- zombieFront.js
+++ zombieFront.js
@@ -1,47 +1,32 @@
-// Así es como accederiamos a nuestro contrato:
 var abi = /* abi generado por el compilador */
-var ZombieFactoryContract = web3.eth.contract(abi)
-var contractAddress = /* nuestra dirección del contrato en Ethereum después de implementarlo */
-var ZombieFactory = ZombieFactoryContract.at(contractAddress)
-// `ZombieFactory` ha accedido a las funciones y eventos públicos de nuestro contrato
+var ZombieFeedingContract = web3.eth.contract(abi)
+var contractAddress = /* la dirección Ethereum de nuestro contrato despues de la implementación */
+var ZombieFeeding = ZombieFeedingContract.at(contractAddress)
 
-// algunos eventos están escuchando para recoger el valor del texto:
-$("#ourButton").click(function(e) {
-  var name = $("#nameInput").val()
-  // Llama a la función `createRandomZombie` de nuestro contrato:
-  ZombieFactory.createRandomZombie(name)
+// Asumiendo que tenemos la ID de nuestro zombi y la ID del gato que queremos atacar
+let zombieId = 1;
+let kittyId = 1;
+
+// Para conseguir la imagen del CryptoKitty, necesitamos hacer una consulta a su API.
+// Esta información no está guardada en la blockchain, solo en su servidor web.
+// Si todo se guardase en la blockchain, no nos tendríamos que preocupar
+// si el servidor se cae (apaga), si cambian la API, o si la compañía 
+// nos bloquea la carga de imágenes si no les gusta nuestro juego de zombis ;)
+let apiUrl = "https://api.cryptokitties.co/kitties/" + kittyId
+$.get(apiUrl, function(data) {
+  let imgUrl = data.image_url
+  // haz algo para enseñar la imagen
 })
 
-// Escucha al evento de `NewZombie`, y actualiza la interfaz
-var event = ZombieFactory.NewZombie(function(error, result) {
+// When the user clicks on a kitty:
+$(".kittyImage").click(function(e) {
+  // Llama al método `feedOnKitty` de tu contrato
+  ZombieFeeding.feedOnKitty(zombieId, kittyId)
+})
+
+// Escuchamos el evento del NewZombie de nuestro contrato para que podamos mostrarlo:
+ZombieFactory.NewZombie(function(error, result) {
   if (error) return
+  // Esta función mostrará el zombi, como en la lección 1:
   generateZombie(result.zombieId, result.name, result.dna)
 })
-
-// Recogemos el adn del zombi y actualizamos nuestra imagen
-function generateZombie(id, name, dna) {
-  let dnaStr = String(dna)
-  // rellenamos el ADN con ceros si es menor de 16 caracteres
-  while (dnaStr.length < 16)
-    dnaStr = "0" + dnaStr
-
-  let zombieDetails = {
-    // los primeros 2 dígitos hacen la cabeza. Tenemos 7 posibles cabezas, entonces hacemos % 7
-    // para obtener un número entre 0 - 6, después le sumamos 1 para hacerlo entre 1 - 7. Tenemos 7
-    // imagenes llamadas desde "head1.png" hasta "head7.png" que cargamos en base a 
-    // este número:
-    headChoice: dnaStr.substring(0, 2) % 7 + 1,
-    // Los siguientes 2 dígitos se refieren a los ojos, 11 variaciones:
-    eyeChoice: dnaStr.substring(2, 4) % 11 + 1,
-    // 6 variaciones de camisetas:
-    shirtChoice: dnaStr.substring(4, 6) % 6 + 1,
-    // los últimos 6 digitos controlas el color. Actualiza el filtro CSS: hue-rotate
-    // que tiene 360 grados:
-    skinColorChoice: parseInt(dnaStr.substring(6, 8) / 100 * 360),
-    eyeColorChoice: parseInt(dnaStr.substring(8, 10) / 100 * 360),
-    clothesColorChoice: parseInt(dnaStr.substring(10, 12) / 100 * 360),
-    zombieName: name,
-    zombieDescription: "A Level 1 CryptoZombie",
-  }
-  return zombieDetails
-}
